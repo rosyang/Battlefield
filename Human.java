6f6f4fba0c41735366f5452eae6425a4f2c435f3
dazigemm <dazigemm@gmail.com>
2014-01-19 17:31:09 -0500

    updates?

--- Human.java
+++ Human.java
@@ -2,21 +2,28 @@ public class Human extends Player {
     private String _name;
 
     public Human () {
-	_numShips = 10;
-	_board = new Tiles();
+	super();
+	//_numShips = 10;
+	//_board = new Tile[10][10];
     }
     public Human (String name) {
-	this;
+	this();
 	_name = name;
     }
 
-    public void attack (Player opp, int x, int y) {
-	System.out.print();
-	Comparaable square = opp._board.get(x,y);
+    public void attack (Player opp, int r, int c) {
+	
+	Tile  t = opp._board[r + 1][c + 1]; /*
 	if ( instanceOf Ships) {
-
+	    .remove(
 	}
 	else if ( instance of ) {
 	}
+	else {
+	}*/
+    }
+    public static void main (String[] args) {
+	Human person = new Human();
+	System.out.println(person);
     }
 }
--- Player.java
+++ Player.java
@@ -1,13 +1,46 @@
+//import java.util.ArrayList;
 public abstract class Player {
 
     //instance vars
     protected int _numShips;
     protected boolean _isTurn;
-    protected Tiles _board;
+    protected  Tile[][] _board;
+    
+    public Player () {
+	_numShips = 10;
+	_board = new Tile[10][10];/*
+	for (int x = 1; x < _board[1].length; x++) {
+	    _board[1][x] = x;
+	}
+	for (int x = 1; x < _board.length; x++) {
+	    _board[x][1] = x;
+	    }*/
+    }
 
     public boolean hasShips() {
-	return numShips > 0;
+	return _numShips > 0;
     }
     
-    public abstract void attack(Player opp, int x, int y) {}
+    public abstract void attack(Player opp, int x, int y);
+
+    public String toString () {
+	String ret = "  1 2 3 4 5 6 7 8 9 10\n";
+	for (int x = 0; x < _board.length; x++ ) {
+	    ret += x + 1 + " ";
+	    for (Tile t : _board[x]) {
+		ret += t + " " ;
+	    }
+	    ret += "\n";
+	}
+	return ret;
+    }
+    public void place (int r, int c) {
+	_board[r][c] = new Tile( );
+    }
+    /*
+    public static void main (String[] args) {
+	Player trial = new Player();//won't work, abstract can't be instantiated
+	System.out.println(trial);
+	}*/
+
 }
