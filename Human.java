1e19b7689b9bd1e9d7089d6a1a016f852f0dac75
rosyang <yang.xy9@gmail.com>
2014-01-21 01:24:07 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -9,7 +9,6 @@ public class Human extends Player {
 
     //========== Default Constructor ==========//
     public Human () {
-<<<<<<< HEAD
 	_battlefield = new Board();
 	_numShip = 5;
 	_isTurn = false;
@@ -39,26 +38,4 @@ public class Human extends Player {
 
     public void specialAttack (Player opp, int x, int y) {}
 
-=======
-	super();
-    }
-    public Human (String name) {
-        this();
-	_name = name;
-    }
-   
->>>>>>> 942a62026b3417227270b81112ad4b717185979a
-    public static void main (String[] args) {
-	Player person = new Human();
-	//System.out.println(person);
-	//person._board[1][2] = new Tile("ship" , 1, "-x-");
-	person.place(1,2,new Tile("ship", 1, "-x-"));
-	person._board[0][1].flip();
-	System.out.println("before");
-	System.out.println(person);
-        Player opp = new Human();
-	opp.attack(person, 1, 2);
-	System.out.println("after");
-	System.out.println(person);
-    }
 }
