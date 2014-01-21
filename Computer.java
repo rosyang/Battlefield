8ce0691bf8cedb87d4399b6fad981a0ddbbd5034
rosyang <yang.xy9@gmail.com>
2014-01-21 01:22:14 -0500

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -5,7 +5,6 @@
 
 public class Computer extends Player {
     
-<<<<<<< HEAD
     //private _guess;
 
 
@@ -27,20 +26,4 @@ public class Computer extends Player {
     public void normalAttack (Player opp, int x, int y) {}
     public void specialAttack (Player opp, int x, int y) {}
 
-=======
-    //private _guess; possibly get all coords & guess by chance?
-    
-
-    public Computer() {
-	super();
-	_name = "Computer";
-    }
-
-    public int guessR () {
-	return 10 + (int) Math.random();
-    }
-    public int guessC () {
-	return 10 + (int) Math.random();
-    }
->>>>>>> 942a62026b3417227270b81112ad4b717185979a
 }
