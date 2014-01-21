9fe20cb96c0bcc8801e31dacbd2faa4d5e0a0195
rosyang <yang.xy9@gmail.com>
2014-01-21 00:47:03 -0500

    add Bonuses

---
+++ Bonuses.java
@@ -0,0 +1,10 @@
+/*
+public class Bonuses{
+
+    //Constructor
+    public Bonuses() {
+	Tile b1 = new Tile( "extra range", 1, "-O-" );
+      	Tile b2 = new Tile( "something else", 1, "-O-" );
+    }
+
+}
